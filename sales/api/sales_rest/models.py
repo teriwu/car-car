f290a66b7948daf78bfeb0921732bd2f3558ae89
Jennifer <xjenniferwux@gmail.com>
2022-08-02 15:37:02 -0700

    Fix __str__ error

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class AutomobileVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    vin = models.CharField(max_length=17)
+
+
+class SalesPerson(models.Model):
+    name = models.CharField(max_length=100)
+    employee_number = models.PositiveIntegerField(unique=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_api_url(self):
+        return reverse("api_sales_person", kwargs={"pk": self.id})
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+    def get_api_url(self):
+        return reverse("api_customer", kwargs={"pk": self.id})
+
+
+class SalesRecord(models.Model):
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name="sales_record",
+        on_delete=models.CASCADE,
+    )
+    sales_person = models.ForeignKey(
+        SalesPerson,
+        related_name="sales_record",
+        on_delete=models.CASCADE,
+    )
+    customer = models.ForeignKey(
+        Customer,
+        related_name="sales_record",
+        on_delete=models.CASCADE,
+    )
+    sales_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def get_api_url(self):
+        return reverse("api_sales_record", kwargs={"pk": self.id})
