a0819f26f7b7c28e5ac8ab803b5a9e546f9a77aa
Teresa Wu <teresa.wu.jy@gmail.com>
2022-08-02 15:18:30 -0700

    api_technician GET and POST working

--- service/api/service_rest/urls.py
+++ service/api/service_rest/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from .views import (
     api_appointments,
     api_appointment,
+    api_technicians,
 )
 
 urlpatterns = [
     path("appointments/", api_appointments, name="api_appointments"),
-    path("appointments/<int:pk>/", api_appointment, name="api_appointment")
+    path("appointments/<str:vin>/", api_appointment, name="api_appointment"),
+    path("technicians/", api_technicians, name="api_technicians"),
 ]
