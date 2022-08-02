999a289fb9427b598b7eb657510c68e235f34b13
Teresa Wu <teresa.wu.jy@gmail.com>
2022-08-01 18:50:41 -0700

    Created models and ran migrations

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class AutomobileVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    vin = models.CharField(max_length=17)
+
+
+class Technician(models.Model):
+    name = models.CharField(max_length=100)
+    employee_number = models.PositiveSmallIntegerField()
+
+
+class Appointment(models.Model):
+    vin = models.CharField(max_length=17)
+    customer_name = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    reason = models.CharField(max_length=200)
+    technician = models.ForeignKey(
+        Technician,
+        related_name="appointments",
+        on_delete=models.CASCADE,
+    )
