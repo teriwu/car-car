74013a5082e547f3f91980b882a37502b0b2691b
Jennifer <xjenniferwux@gmail.com>
2022-08-02 15:40:14 -0700

    Register Admin classes

--- sales/api/sales_rest/admin.py
+++ sales/api/sales_rest/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import AutomobileVO, SalesPerson, SalesRecord
 
 # Register your models here.
+@admin.register(AutomobileVO)
+class AutomobileVOAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(SalesPerson)
+class SalesPersonAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(SalesRecord)
+class SalesRecordAdmin(admin.ModelAdmin):
+    pass
