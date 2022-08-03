e4e86ad20fe1b2a6f02d72769d02426252a907b2
Jennifer <xjenniferwux@gmail.com>
2022-08-02 18:33:56 -0700

    Add GET and POST for customers

--- sales/api/sales_rest/api_urls.py
+++ sales/api/sales_rest/api_urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from .api_views import (
     api_sales_persons,
     api_sales_person,
+    api_customers,
 )
 
 
 urlpatterns = [
     path("sales_persons/", api_sales_persons, name="sales_persons"),
     path("sales_persons/<int:pk>/", api_sales_person, name="sales_person"),
+    path("customers/", api_customers, name="customers"),
 ]
