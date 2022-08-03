5d67a7bb85b7980ad4d485c766e07f2c7dc32374
Teresa Wu <teresa.wu.jy@gmail.com>
2022-08-02 18:20:06 -0700

    Updated Technician and Appointment models

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
@@ -9,17 +10,24 @@ class AutomobileVO(models.Model):
 
 class Technician(models.Model):
     name = models.CharField(max_length=100)
-    employee_number = models.PositiveSmallIntegerField()
+    employee_number = models.AutoField(primary_key=True)
+
+    def get_api_url(self):
+        return reverse("api_technician", kwargs={"pk": self.pk})
+
 
 
 class Appointment(models.Model):
+    vip = models.BooleanField(default=False)
     vin = models.CharField(max_length=17)
     customer_name = models.CharField(max_length=100)
-    date = models.DateField()
-    time = models.TimeField()
+    date_time = models.DateTimeField()
     reason = models.CharField(max_length=200)
     technician = models.ForeignKey(
         Technician,
         related_name="appointments",
         on_delete=models.CASCADE,
     )
+
+    def get_api_url(self):
+        return reverse("api_appointment", kwargs={"pk": self.pk})
