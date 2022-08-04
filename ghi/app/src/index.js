05e1a6b45dbf6619340bfa1245baec8e183db55d
Teresa Wu <teresa.wu.jy@gmail.com>
2022-08-03 18:35:43 -0700

    Properly fetching from automobiles api

--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+async function loadCarCar() {
+  let serviceData;
+  const serviceResponse = await fetch("http://localhost:8080/api/appointments/");
+  
+  if (serviceResponse.ok) {
+    serviceData = await serviceResponse.json();
+    console.log("service data:", serviceData)
+  } else {
+    console.error(serviceResponse);
+  }
+
+  root.render(
+    <React.StrictMode>
+      <App appointments={serviceData.appointments} />
+    </React.StrictMode>
+  );
+}
+loadCarCar();
