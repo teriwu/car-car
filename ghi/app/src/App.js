7f2c6ea19bd60594ce5db1a128d3d3add12d06a9
Teresa Wu <teresa.wu.jy@gmail.com>
2022-08-04 11:53:54 -0700

    Fixed path to TechnicianForm

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,14 +1,29 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import AppointmentForm from "./AppointmentForm";
+import AppointmentsList from "./AppointmentList";
+import TechnicianForm from "./TechnicianForm";
 
-function App() {
+
+function App(props) {
+  // if (props.appoints === undefined) {
+  //   return null;
+  // }
+
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="appointments">
+            <Route index element={<AppointmentsList appointments={props.appointments} />}/>
+            <Route path="new" element={<AppointmentForm />} />
+          </Route>
+          <Route path="technicians">
+            <Route path="new" element={<TechnicianForm />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
